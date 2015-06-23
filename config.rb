6063fbd1c11649e021c1cabbd4e9acadeb6690bd
Dayton Nolan <daytonn@gmail.com>
2015-06-23 10:38:41 -0500

    Update configuration

--- config.rb
+++ config.rb
@@ -58,6 +58,6 @@ activate :syntax
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
-  activate :relative_assets
+  # activate :asset_hash
+  # activate :relative_assets
 end
